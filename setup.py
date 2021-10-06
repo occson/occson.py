85652ade9c70ca178dcdcc750a3f4ec92d5a0d36
Tomasz Kowalewski <me@tkowalewski.pl>
2021-10-06 19:59:19 +0200

    Bumped version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
   name="occson",
-  version="0.1.0",
+  version="1.0.0",
   author="Tomasz Kowalewski",
   author_email="me@tkowalewski.pl",
   maintainer="Tomasz Kowalewski",
